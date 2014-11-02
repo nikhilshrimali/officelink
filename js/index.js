8cf12f2b73bc75d46cfc00d8d1794fe97a215396
Nikhil Shrimali <ernikhilshrimali@gmail.com>
2014-11-02 10:37:09 +0530

    add new js and some changes in html and css fiels

--- js/index.js
+++ js/index.js
@@ -1,9 +1,33 @@
-function copyText() {
-  var output = document.getElementById('linkarea').value;
-   document.getElementById('slider').innerHTML = output;
-   console.log (output);
- }
+// Get DOM Elements
+var textContent = document.getElementById('text-content');
+var outputBox = document.getElementById('output-box');
+
+
+
+function textToLinks(text) {
+  var links =  text.split(" ");
+  for (var i=0; i<=links.length-1; i++) {
+    if ( isLink (links [i]) == false ) {
+      links.splice (i, 1);
+      i--;
+    }
+  }
+  return links;
+}
+
+
+function isLink (str) {
+  var value = false;
+  if (str == "http") {
+    value = true;
+  }
+  else if (str == "HTTP") {
+    value = true;
+  }
+  return value;
+}
+
 
 document.addEventListener ("keyup", function () {
-  copyText ();
+  console.log ("Links:" + textToLinks (textContent.value));
 }, false);
